4c12769c9e8dbb0f4f7c544f2c19c1baeca19934
Ashutosh Shandilya <162418627+ashutoshshandilya7973@users.noreply.github.com>
2025-01-09 00:21:43 +0530

    Update db.js
    database urlchange

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
 
 export const connectDB=async()=>{
-     await mongoose.connect('mongodb+srv://ashutoshjha7973:BamBam7973@cluster0.p9mum.mongodb.net/ashu-ecom').then(()=>console.log("DB connected"));
+     await mongoose.connect('Database_Url').then(()=>console.log("DB connected"));
 }
